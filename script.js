bd7e38e5b764aeabb72923867018ad7c4c8dcd92
ryan woods <federer5050@gmail.com>
2020-10-16 17:05:49 +0100

    initial commit

--- script.js
+++
@@ -1,6 +0,0 @@
-const menuicon = document.querySelector('.hamburger-menu');
-const navbar = document.querySelector('.navbar');
-
-menuicon.addEventListener('click', () => {
-    navbar.classList.toggle('change');
-});
